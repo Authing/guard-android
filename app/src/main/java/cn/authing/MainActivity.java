80f9460ba5281eb9b5910262a94d6609c19798f7
Lance Mao <maolongdong@gmail.com>
2022-05-11 09:11:50 +0800

    handle delete account event in app

--- app/src/main/java/cn/authing/MainActivity.java
+++ app/src/main/java/cn/authing/MainActivity.java
@@ -1,10 +1,11 @@
 package cn.authing;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
-import cn.authing.guard.Authing;
 import cn.authing.guard.activity.UserProfileActivity;
 import cn.authing.guard.network.AuthClient;
 
@@ -14,8 +15,8 @@ public class MainActivity extends UserProfileActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button btn = findViewById(R.id.btn_logout);
-        btn.setOnClickListener(v -> logout());
+        findViewById(R.id.btn_logout).setOnClickListener(v -> logout());
+        findViewById(R.id.btn_delete).setOnClickListener(v -> delete());
     }
 
     private void logout() {
@@ -24,5 +25,18 @@ public class MainActivity extends UserProfileActivity {
             startActivity(intent);
             finish();
         });
+    }
+
+    private void delete() {
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle(cn.authing.guard.R.string.authing_delete_account).setMessage(cn.authing.guard.R.string.authing_delete_account_tip)
+                .setPositiveButton(android.R.string.yes, (dialog, which) -> AuthClient.deleteAccount((code, message, data) -> {
+                    if (code == 200) {
+                        finish();
+                    } else {
+                        runOnUiThread(()-> Toast.makeText(this, message, Toast.LENGTH_LONG).show());
+                    }
+                }))
+                .setNegativeButton(android.R.string.no, null).show();
     }
 }
